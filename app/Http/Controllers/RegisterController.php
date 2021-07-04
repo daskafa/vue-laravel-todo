f6aa3c45ff15e185be274ae7b2893ae9c1fee751
Yılmaz Daşkafa <ydaskafa@gmail.com>
2021-07-04 19:52:32 +0300

    RegisterController and routes created

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function register(Request $request){
+        $request->validate([
+            'name' => 'required|min:3',
+            'email' => 'required|email|min:3|unique:users',
+            'password' => 'required|min:3|confirmed'
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+    }
+}
