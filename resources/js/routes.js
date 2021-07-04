1f8e46023ab5d39c37b8ddf7167d779d219cb854
Yılmaz Daşkafa <ydaskafa@gmail.com>
2021-07-04 20:26:09 +0300

    The routes we will need are created in routejs

---
+++ resources/js/routes.js
@@ -0,0 +1,43 @@
+import Home from './components/Home';
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
+
+import NotFound from "./components/NotFound";
+
+export default {
+    mode: 'history',
+    linkActiveClass: 'font-semibold',
+    routes: [
+        {
+            path: '*',
+            component: NotFound
+        },
+        {
+            path: '/',
+            component: Home,
+            name: 'Home'
+        },
+        {
+            path: '/login',
+            component: Login,
+            name: 'Login'
+        },
+        {
+            path: '/register',
+            component: Register
+        },
+        {
+            path: '/dashboard',
+            component: Dashboard,
+            name: 'Dashboard',
+            beforeEnter: (to, form, next) => {
+                axios.get('/api/authenticated').then(() => {
+                    next()
+                }).catch(() => {
+                    return next({name: 'Login'})
+                })
+            }
+        }
+    ]
+}
