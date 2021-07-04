e2919012a0e0768cc038c6b83b80bf2d367e24af
Yılmaz Daşkafa <ydaskafa@gmail.com>
2021-07-04 21:53:22 +0300

    NotFound and Home components created

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container my-3">
+        <p class="text-white text-center">Home Page</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
